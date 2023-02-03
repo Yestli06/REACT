d803d3aa0d6cb10ee429009c3846158f6c09ba1f
SENA <SENA@BOGSIS04198>
2023-02-03 16:00:42 -0500

    calculadora nueva

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,42 +1,73 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Contador  from './contador'
-import Yes from './contador'
+import Example from './contador'
 
-function App() {
-  const Nuevo=()=>{
-    
-    return (
-      <p>Nuevo componente</p>
+
+function App() {  
+  const [valor, setValor] = useState("")
+  const [valor2] = useState("")
+
+  var resultado;
+
+  var suma;
+  function Example(props){
+    return(
+       <button onClick={()=> setValor(valor +(props.numero) )} class="btn btn-danger"> {props.numero}</button>  
+      )
+  }
+
+  
+
+  const Pantalla=()=>{
+    return(
+        <input type="text" value={valor} />
     )
   }
 
-const Aler=()=>{alert("Mensaje de alerta")
-}
+  const Pantalla2=()=>{
+    return(
+        <input type="text" value={valor2} />
+    )
+  }
 
-const Otro=()=>{
-return (
-  //(<button onClick={()=>(console.log("boton click"))} >nuevo componente</button>)
-  (<button onClick={()=>(Aler())} >nuevo componente</button>)
-)
-
-}
+  const Suma=()=>{
+   
+  }
 
 
-const b="esto es una prueba"
-var c="variable con var"
-
-return (
-  <>
-    {/* <h1> { Nuevo}</h1>
-    <h2> Prueba spa { b } </h2>
-    <h1>{c}</h1>
-    <Otro/> */}
-    <Contador/>
-  
-  </>
-)
+  return (
+    <>
+      <Pantalla/><br />
+      <Pantalla2/>
+      <table border="1">
+     <tr>
+        
+     </tr>
+      <tr>
+        <td><Example numero="1"/></td>
+        <td><Example numero="2"/></td>
+        <td><Example numero="3"/></td>
+        <td><Example numero="4"/></td>
+        <td><Suma/>+</td>
+      </tr>
+      <tr>
+        <td><Example numero="5"/></td>
+        <td><Example numero="6"/></td>
+        <td><Example numero="7"/></td>
+        <td><Example numero="8"/></td>
+        <td><Example numero="-"/></td>
+      </tr>
+      <tr>
+        <td><Example numero="9"/></td>
+        <td><Example numero="0"/></td>
+        <td><Example numero="/"/></td>
+        <td><Example numero="*"/></td>
+        <td><Example numero="="/></td>
+      </tr>
+    </table>
+    </>
+  )
 
 }
 
